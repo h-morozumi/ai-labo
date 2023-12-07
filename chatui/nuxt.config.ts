94b791029da2b3a9ed22b7b71285d03bc4b47ac6
h-morozumi <morozumi.h@outlook.com>
2023-12-07 13:13:08 +0000

    必要な設定ファイルを準備

--- chatui/nuxt.config.ts
+++ chatui/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  runtimeConfig: {
+    public: {
+      //Public runtime config here
+    }
+    // Server runtime config here
+  }
 })
