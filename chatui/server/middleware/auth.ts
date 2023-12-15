6ef82b0b33848423ed1af2c561e337edbffdb270
h-morozumi <morozumi.h@outlook.com>
2023-12-15 05:36:06 +0000

    APIの認証処理を共通化

--- chatui/server/middleware/auth.ts
+++ chatui/server/middleware/auth.ts
@@ -1,12 +1,11 @@
-import { defineEventHandler, send } from "h3";
+import { defineEventHandler, createError } from "h3";
 
 // Microsoft Graph API の URL
 const graphApiUrl = "https://graph.microsoft.com/v1.0/me";
 
-// イベントハンドラーを定義します。
 export default defineEventHandler(async (event) => {
-  const authUser = event.context.user;
-  console.log(`authUser: ${JSON.stringify(authUser)}`);
+  // クライアントサイドでは実行しないようにする（本当は next.config.ts で設定できるはずだがうまくいかない)
+  if (!event.path.startsWith("/api")) return;
 
   // Authorization ヘッダーを取得します。
   const authorization = getHeader(event, "Authorization");
@@ -14,26 +13,37 @@ export default defineEventHandler(async (event) => {
   // Authorization ヘッダーが存在しない、または Bearer トークンが含まれていない場合、エラーを返します。
   if (!authorization || !authorization.startsWith("Bearer ")) {
     console.error("Authorization header is missing or is not a Bearer token.");
-    throw createError({ statusCode: 401, statusMessage: 'Authorization Error' });
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Authorization Error",
+    });
   }
 
   // Bearer トークンを抽出します。
   const token = authorization.replace("Bearer ", "");
 
-   try {
+  const userData = await verifyToken(token);
+  if (!userData) {
+    throw createError({ statusCode: 401, statusMessage: "Invalid Token" });
+  }
+  event.context.user = userData;
+});
+
+async function verifyToken(token: string) {
+  try {
     // Fetch API を使用してユーザー情報を取得します。
     const user = await $fetch(graphApiUrl, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-
-    // 成功した場合、ユーザー情報をJSON形式で返します。
-    await send(event, JSON.stringify(user), "application/json");
+    return user;
   } catch (error) {
     // エラーが発生した場合、エラーメッセージを返します。
     console.error("Data fetch failed:", error);
-    throw createError({ statusCode: 401, statusMessage: 'Authorization Error' });
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Authorization Error",
+    });
   }
-});
-
+}
