86f832df837bebfdd3be2364b83f53a2d53e9f5e
h-morozumi <morozumi.h@outlook.com>
2024-01-10 09:38:41 +0000

    profile情報移動

--- chatui/layouts/default.vue
+++ chatui/layouts/default.vue
@@ -3,6 +3,7 @@
         <NuxtLink to="/">Home</NuxtLink>&nbsp;|&nbsp;
         <NuxtLink to="/avatar">Avatar</NuxtLink>&nbsp;|&nbsp;
         <NuxtLink to="/about">About</NuxtLink>&nbsp;|&nbsp;
+        <NuxtLink to="/profile">Profile</NuxtLink>&nbsp;|&nbsp;
         <NuxtLink to="/chat">Chat</NuxtLink>
     </div>
     <slot />
---
+++ chatui/pages/profile.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Welcome to your 365 profile! 🌟</h1>
+        <UserProfile />
+        <hr />
+        <MSInfo />
+    </div>
+</template>
